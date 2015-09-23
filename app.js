3414d4935c80dd40e1afc5f84d8963b6fd57ab93
fung <fung@macbook>
2015-09-23 23:45:34 +0800

    ADD mqtt poc

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+
+var mqtt = require('mqtt');
+
+var options = {
+  port: 1883,
+  host: '192.168.1.100',
+  clientId:'Node_mqtt_client_001'
+};
+
+client = mqtt.connect(options);
+
+
+client.subscribe('/status');
+client.on('message', function (topic, message) {
+  console.log(message.toString());
+  io.emit('chat message', message.toString());
+});
+
  
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'index.html'));
@@ -14,10 +32,13 @@ io.on('connection', function(socket){
     console.log('user disconnected');
   });
     
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
- 
-    io.emit('chat message', msg);
+  socket.on('cmd',function(msg){
+  	var obj = JSON.parse(msg.toString());
+
+  	var clientId = obj.clientId;
+  	delete obj.clientId;
+    console.log(msg.toString())
+  	client.publish('/cmd/'+clientId,JSON.stringify(obj));
   });
  
 });
